37c46d6cc55dcdf806735145608e10066b945b60
Saulo Christian <saullochristian@outlook.com>
2024-05-21 18:19:02 -0300

    Estilização do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
 import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Saulo Christian"
         content="Lorem Ipsum" 
